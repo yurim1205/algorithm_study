b8096fc058cb54cbb0875e6b86b3efc6c836458b
yurim <93933759+yurim1205@users.noreply.github.com>
2025-04-23 01:22:12 +0900

    [Bronze V] Title: A-B, Time: 96 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : "/Users/iyulim/Desktop/algorithm/input.txt";
+
+const inputData = fs.readFileSync(filePath, 'utf-8').trim().split(' ');
+
+const A = parseInt(inputData[0]);
+const B = parseInt(inputData[1]);
+
+console.log(A - B);
