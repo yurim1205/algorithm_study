3d24a229cb6b3f69d4037af231dc3fe48097cf82
yurim <93933759+yurim1205@users.noreply.github.com>
2025-04-23 00:22:31 +0900

    [Bronze V] Title: A+B - 7, Time: 96 ms, Memory: 9504 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,15 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "/Users/iyulim/Desktop/algorithm/input.txt")
+  .toString()
+  .split('\n');
+
+  const inputCase = Number(input[0]);
+
+  for (let i = 1; i <= inputCase; i++) {
+    let num = input[i].split(' ');
+    let num1 = Number(num[0]);
+    let num2 = Number(num[1]);
+    let result = num1 + num2;
+
+    console.log(`Case #${i}: ${result}`);
+  }
